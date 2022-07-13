1e495c98fb89c6ab1d22ccadbd86924828d18189
Brandon Madison <brandmadis@gmail.com>
2022-07-12 17:40:36 -0700

    add admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Ask_it
 
-# Register your models here.
+class Ask_itAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Ask_it, Ask_itAdmin)
